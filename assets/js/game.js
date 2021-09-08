5ecb1d912ab4c66154aca4d33a659f10203c7f0e
TJ James <jamestw13@gmail.com>
2021-09-08 14:45:00 -0500

    added randomized health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,14 +37,16 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
+
     console.log(
       playerName +
         " attacked " +
@@ -70,7 +72,8 @@ var fight = function (enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName +
         " attacked " +
@@ -109,7 +112,7 @@ var startGame = function () {
       var pickedEnemyName = enemyNames[i];
 
       //reset enemy health
-      enemyHealth = 20;
+      enemyHealth = randomNumber(40, 60);
 
       // debugger option
       // debugger;
@@ -200,6 +203,13 @@ var shop = function () {
       shop();
       break;
   }
+};
+
+// Number Randomizer
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (60 - 40 + 1)) + min;
+
+  return value;
 };
 
 // Start the game
